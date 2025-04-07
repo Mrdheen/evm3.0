848edb6ec360dad16725b945e5f09779e47f8d0a
Mrdheen <shamsubinbello@gmail.com>
2025-04-07 18:01:31 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,15 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <title>EVM</title>
+
+        {/* Favicon for browser tab */}
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
+        {/* Optional: Apple touch icon for iOS devices */}
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
       <body>
-          <Navbar />
+        <Navbar />
         {children}
         <Footer />
       </body>
